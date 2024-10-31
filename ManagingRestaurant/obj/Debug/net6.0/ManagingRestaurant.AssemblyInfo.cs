30248e16a2f0e17f51a4461ee96a4452407eb64c
PhuongNam277 <namp3411@gmail.com>
2024-10-31 10:24:41 +0700

    first push

--- ManagingRestaurant/obj/Debug/net6.0/ManagingRestaurant.AssemblyInfo.cs
+++ ManagingRestaurant/obj/Debug/net6.0/ManagingRestaurant.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagingRestaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a8b724828f44a4937aa2710956399b71140ecb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d07e3fa505fc2f40f32bc44238dfcc3039ed749")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagingRestaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagingRestaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
