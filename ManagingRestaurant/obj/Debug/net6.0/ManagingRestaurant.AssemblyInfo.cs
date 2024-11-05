9ac98460e0132df8ea5e6d1ec2bb44b8334ccb24
CodeWithAnn <tronganfwork@gmail.com>
2024-11-05 20:49:34 +0700

    pull

--- ManagingRestaurant/obj/Debug/net6.0/ManagingRestaurant.AssemblyInfo.cs
+++ ManagingRestaurant/obj/Debug/net6.0/ManagingRestaurant.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagingRestaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c21a6d924dbd8869b258bcae5088a34b75e22165")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+594d61e1fc605a6ab706bcc196858f871276f44e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagingRestaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagingRestaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
